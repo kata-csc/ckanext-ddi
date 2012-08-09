55ad47be661345c192fb16c7fb2752916d6298bd
Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>
2012-08-09 19:29:39 +0300

    Fix to only get leafs from metadata.

--- ckanext/ddi/harvester.py
+++ ckanext/ddi/harvester.py
@@ -85,7 +85,7 @@ class DDIHarvester(SingletonPlugin):
 
     def _combine_and_flatten(self, xml_dict):
         res = {}
-        for els in etree.fromstring(xml_dict).xpath('//*[not(child::*)]'):
+        for els in etree.fromstring(xml_dict).xpath('//stdyDscr//*[not(child::*)]|//docDscr//*[not(child::*)]'):
             if not els.tag in res:
                 res[els.tag] = els.text if els.text else self._collect_attribs(els)
             else:
